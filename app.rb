ad127644c17102c53a4dcc0d19fe1c56fbea8584
sbb3391 <sbb33391@gmail.com>
2021-02-03 23:41:14 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,21 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
 
+    erb :newteam
+  end
+
+  post '/newteam' do 
+    x = params
+    @team_name = x[:name]
+    @coach = x[:coach]
+    @pg = x[:pg]
+    @sg = x[:sg] 
+    @sf = x[:sf]
+    @pf = x[:pf]
+    @c = x[:c]
+    erb :team
+  end
 end
+
